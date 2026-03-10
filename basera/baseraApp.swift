cff51b14932cb820967bc9edb325521ca85d3678
Pramod Paudel <account@pramodpoudel.com.np>
2026-03-10 19:28:49 +0545

    Initial Commit

---
+++ basera/baseraApp.swift
@@ -0,0 +1,17 @@
+//
+//  baseraApp.swift
+//  basera
+//
+//  Created by Pramod Paudel on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct baseraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
